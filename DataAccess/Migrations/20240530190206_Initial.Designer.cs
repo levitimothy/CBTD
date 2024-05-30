abe9bff2cbcb09a22c647b0171d82cff06a3ea59
levitimothy <levitimothy@mail.weber.edu>
2024-05-30 13:04:40 -0600

    finished assignement

--- DataAccess/Migrations/20240530190206_Initial.Designer.cs
+++ DataAccess/Migrations/20240530190206_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240522183246_Initial")]
+    [Migration("20240530190206_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
